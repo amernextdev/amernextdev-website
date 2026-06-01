79595af3f070a37e74f5192225d6f9bf377014c0
amer <iamamer030@gmail.com>
2026-06-01 18:04:54 +0300

    add fonts.css, vriables.css

---
+++ dev/src/constants/index.js
@@ -0,0 +1,18 @@
+// /src/constants/index.js
+export const THEMES = { DARK: 'dark', LIGHT: 'light' };
+export const LANGUAGES = ['ar', 'en'];
+export const DEFAULT_LANG = 'en';
+export const DEFAULT_THEME = 'dark';
+
+export const LOCAL_STORAGE_KEYS = {
+  THEME: 'theme',
+  LANG: 'lang'
+}
+
+export const SECTIONS = {
+  HERO:     '#hero',
+  WORK:     '#work',
+  SERVICES: '#services',
+  ABOUT:    '#about',
+  CONTACT:  '#contact'
+}
