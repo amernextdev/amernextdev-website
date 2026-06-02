5cfa20a0547ecc550ee45760968e34c824946739
amer <iamamer030@gmail.com>
2026-06-02 06:26:40 +0300

    complated i18n.js

--- dev/src/main.js
+++ dev/src/main.js
@@ -7,5 +7,13 @@ import './style.css';
 
 // === LOGIC ===
 // - Services
-import { initI18n } from '/src/services/js/i18n.js';
-import { initTheme } from '/src/services/js/theme.js';
+import { initI18n, prefetchLocales } from '/src/services/js/i18n.js';
+// import { initTheme } from '/src/services/js/theme.js';
+
+
+
+
+await initI18n();
+requestIdleCallback(() => 
+  prefetchLocales(document.documentElement.lang === 'en' ? 'ar' : 'en')
+);
