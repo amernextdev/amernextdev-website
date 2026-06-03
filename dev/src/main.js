1275c2bb59303aeab5635b2a801beb6ace8f5851
amer <iamamer030@gmail.com>
2026-06-03 10:33:11 +0300

    تم التصميم المبدئي للهيدر وجاري العمل على الشعار

--- dev/src/main.js
+++ dev/src/main.js
@@ -9,7 +9,7 @@ import './style.css';
 // - Services
 import { initI18n, prefetchLocales } from '/src/services/js/i18n.js';
 import { initTheme } from '/src/services/js/theme.js';
-
+import '/src/components/header/header.js';
 
 
 
