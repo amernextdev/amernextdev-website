010a86ef77a3ed87550d7e18634bee92b82b4e30
amer <iamamer030@gmail.com>
2026-06-06 12:45:47 +0300

    feat: complated about sectio and add the scroll-reveal lib

--- dev/src/main.js
+++ dev/src/main.js
@@ -5,13 +5,16 @@
 // === STYLES ===
 import './style.css';
 
+/* === Libs === */
+import '/src/assets/libs/scroll-reveal/scroll-reveal.js';
+
 // === LOGIC ===
 // - Services
 import { initI18n, prefetchLocales } from '/src/services/js/i18n.js';
 import { initTheme } from '/src/services/js/theme.js';
+// - Components
 import { initHeader } from '/src/components/header/header.js';
-
-
+import '/src/components/about/about.js';
 
 
 await initI18n();
